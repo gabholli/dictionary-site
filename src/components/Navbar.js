6276d00aa289025743b81a16653597fa9a29a1c1
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2023-10-05 00:02:26 -0700

    Realigned text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,12 @@ function Navbar(props) {
 
     return (
         <div className="navbar">
-            <h3 className="navbar-text">My Dictionary Site</h3>
+            <h2 className="navbar-text">My Dictionary Site</h2>
             <div className="search-container">
                 <form onSubmit={props.handleSubmit}>
                     <input type="text"
                         placeholder="Search for a word"
-                        // onChange={props.handleChange}
+                        // onChange={props.handleChange}    
                         name="search"
                     // value={props.search}
                     >
